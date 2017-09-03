5fd13f3b7c6fd7e2b245e2e5461fec5490dd35e3
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2017-09-03 23:32:53 +0200

    Fix up lab3 tests

--- lab3/test/lab3_test.exs
+++ lab3/test/lab3_test.exs
@@ -1,61 +1,60 @@
 defmodule Lab3Test do
   use ExUnit.Case
-  use TrueStory
-  alias Lab3.Room
+  alias Lab3.Chat
 
   setup do
-    %{room: Room.new()}
+    %{room: Chat.new()}
   end
 
   test "new room", %{room: room} do
-    assert %Room{} = room
+    assert %Chat{} = room
   end
 
   test "join new member", %{room: room} do
-    room = join(room, "Joe")
-    assert has_member?(room, "Joe")
-    refute has_member?(room, "Jose")
-    assert members(room) == ["Joe"]
+    room = Lab3.join(room, "Joe")
+    assert Lab3.has_member?(room, "Joe")
+    refute Lab3.has_member?(room, "Jose")
+    assert Lab3.members(room) == ["Joe"]
   end
 
   test "join existing member", %{room: room} do
-    room = join(room, "Joe")
+    room = Lab3.join(room, "Joe")
     assert_raise ArgumentError, fn ->
-      join(room, "Joe")
+      Lab3.join(room, "Joe")
     end
   end
 
   test "leave existing member" %{room: room} do
-    room = join(room, "Joe")
-    room = leave(room, "Joe")
-    refute has_member?(room, "Joe")
+    room = Lab3.join(room, "Joe")
+    room = Lab3.leave(room, "Joe")
+    refute Lab3.has_member?(room, "Joe")
   end
 
   test "leave non-existent member" %{room: room} do
     assert_raise ArgumentError, fn ->
-      leave(room, "Joe")
+      Lab3.leave(room, "Joe")
     end
   end
 
   test "broadcast message", %{room: room} do
-    room = join(room, "Joe")
-    room = join(room, "Robert")
-    room = send_messages(room, "Joe", "Hello World")
-    assert messages_to_user(room, "Joe") == ["Hello World"]
-    assert messages_to_user(room, "Robert") == ["Hello World"]
+    room = Lab3.join(room, "Joe")
+    room = Lab3.join(room, "Robert")
+    room = Lab3.send_messages(room, "Joe", "Hello World")
+    assert Lab3.messages_to_user(room, "Joe") == ["Hello World"]
+    assert Lab3.messages_to_user(room, "Robert") == ["Hello World"]
   end
 
   test "messages to user", %{room: room} do
-    room = join(room, "Joe")
-    room = send_message(room, "Robert", "Joe", "Hello Joe")
-    assert messages_to_user(room, "Joe") == ["Hello Joe"]
-    assert messages_to_user(room, "Robert") == []
+    room = Labb3.join(room, "Joe")
+    room = Labb3.send_message(room, "Robert", "Joe", "Hello Joe")
+    assert Labb3.messages_to_user(room, "Joe") == ["Hello Joe"]
+    assert Labb3.messages_to_user(room, "Robert") == []
   end
 
   test "messages from user", %{room: room} do
-    room = join(room, "Joe")
-    room = send_message(room, "Robert", "Joe", "Hello Joe")
-    assert messages_from_user(room, "Robert") == ["Hello Joe"]
-    assert messages_from_user(room, "Joe") == []
+    room = Labb3.join(room, "Joe")
+    room = Labb3.send_message(room, "Robert", "Joe", "Hello Joe")
+    assert Labb3.messages_from_user(room, "Robert") == ["Hello Joe"]
+    assert Labb3.messages_from_user(room, "Joe") == []
   end
 end
