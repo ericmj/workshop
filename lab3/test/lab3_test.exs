5c73399c7d0668e1f4f0223a4f8759fcb6266d00
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2017-09-04 07:41:25 +0200

    Fix up lab3 tests

--- lab3/test/lab3_test.exs
+++ lab3/test/lab3_test.exs
@@ -3,7 +3,7 @@ defmodule Lab3Test do
   alias Lab3.Chat
 
   setup do
-    %{room: Chat.new()}
+    %{room: Lab3.new()}
   end
 
   test "new room", %{room: room} do
@@ -24,13 +24,13 @@ defmodule Lab3Test do
     end
   end
 
-  test "leave existing member" %{room: room} do
+  test "leave existing member", %{room: room} do
     room = Lab3.join(room, "Joe")
     room = Lab3.leave(room, "Joe")
     refute Lab3.has_member?(room, "Joe")
   end
 
-  test "leave non-existent member" %{room: room} do
+  test "leave non-existent member", %{room: room} do
     assert_raise ArgumentError, fn ->
       Lab3.leave(room, "Joe")
     end
@@ -45,16 +45,16 @@ defmodule Lab3Test do
   end
 
   test "messages to user", %{room: room} do
-    room = Labb3.join(room, "Joe")
-    room = Labb3.send_message(room, "Robert", "Joe", "Hello Joe")
-    assert Labb3.messages_to_user(room, "Joe") == ["Hello Joe"]
-    assert Labb3.messages_to_user(room, "Robert") == []
+    room = Lab3.join(room, "Joe")
+    room = Lab3.send_message(room, "Robert", "Joe", "Hello Joe")
+    assert Lab3.messages_to_user(room, "Joe") == ["Hello Joe"]
+    assert Lab3.messages_to_user(room, "Robert") == []
   end
 
   test "messages from user", %{room: room} do
-    room = Labb3.join(room, "Joe")
-    room = Labb3.send_message(room, "Robert", "Joe", "Hello Joe")
-    assert Labb3.messages_from_user(room, "Robert") == ["Hello Joe"]
-    assert Labb3.messages_from_user(room, "Joe") == []
+    room = Lab3.join(room, "Joe")
+    room = Lab3.send_message(room, "Robert", "Joe", "Hello Joe")
+    assert Lab3.messages_from_user(room, "Robert") == ["Hello Joe"]
+    assert Lab3.messages_from_user(room, "Joe") == []
   end
 end
